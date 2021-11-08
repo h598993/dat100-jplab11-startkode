84bfa25b3711fb45d204fffe034314a640fc7ab9
h598993 <89031614+h598993@users.noreply.github.com>
2021-11-08 22:34:10 +0100

    lagt til oppgave 1 til og med 5

--- src/no/hvl/dat100/jplab11/oppgave3/Blogg.java
+++ src/no/hvl/dat100/jplab11/oppgave3/Blogg.java
@@ -5,67 +5,93 @@ import no.hvl.dat100.jplab11.oppgave1.*;
 
 public class Blogg {
 
-	// TODO: objektvariable 
+	// objektvariable
+	private Innlegg[] innleggstabell;
+	private int nesteledig;
 
 	public Blogg() {
-		throw new UnsupportedOperationException(TODO.constructor("Blogg"));
+		innleggstabell = new Innlegg[20];
+		nesteledig = 0;
 	}
 
 	public Blogg(int lengde) {
-		throw new UnsupportedOperationException(TODO.constructor("Blogg"));
+		innleggstabell = new Innlegg[lengde];
+		nesteledig = 0;
 	}
 
 	public int getAntall() {
-		throw new UnsupportedOperationException(TODO.method());
+		return nesteledig;
 	}
-	
+
 	public Innlegg[] getSamling() {
-		throw new UnsupportedOperationException(TODO.method());
+		return innleggstabell;
 
 	}
-	
-	public int finnInnlegg(Innlegg innlegg) {
 
-		throw new UnsupportedOperationException(TODO.method());
+	public int finnInnlegg(Innlegg innlegg) {
+		int pos = 0;
+		while (pos < nesteledig) {
+			if (innleggstabell[pos].erLik(innlegg)) {
+				return pos;
+			}
+			pos++;
+		}
+		return -1;
 	}
 
 	public boolean finnes(Innlegg innlegg) {
-		throw new UnsupportedOperationException(TODO.method());
+		for(int i = 0; i < nesteledig;i++) {
+			if(innleggstabell[i].erLik(innlegg)) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 	public boolean ledigPlass() {
-		throw new UnsupportedOperationException(TODO.method());
-
+		return nesteledig < innleggstabell.length;
 	}
-	
+
 	public boolean leggTil(Innlegg innlegg) {
-
-		throw new UnsupportedOperationException(TODO.method());
+		if(!(finnes(innlegg)) && ledigPlass()) {
+			innleggstabell[nesteledig] = innlegg;
+			nesteledig++;
+			return true;
+		}
+		return false;
 	}
-	
+
 	public String toString() {
-		throw new UnsupportedOperationException(TODO.method());
+		
+		String returStreng = "";
+		
+		for( int i = 0 ; i < nesteledig; i++) {
+			returStreng += innleggstabell[i].toString();
+		}
+		
+		
+		return nesteledig + "\n" + returStreng;  
 	}
 
 	// valgfrie oppgaver nedenfor
-	
+
 	public void utvid() {
 		throw new UnsupportedOperationException(TODO.method());
 	}
-	
+
 	public boolean leggTilUtvid(Innlegg innlegg) {
 
 		throw new UnsupportedOperationException(TODO.method());
-		
+
 	}
-	
+
 	public boolean slett(Innlegg innlegg) {
-		
+
 		throw new UnsupportedOperationException(TODO.method());
 	}
-	
+
 	public int[] search(String keyword) {
-		
+
 		throw new UnsupportedOperationException(TODO.method());
 
 	}
--- src/no/hvl/dat100/jplab11/oppgave5/LesBlogg.java
+++ src/no/hvl/dat100/jplab11/oppgave5/LesBlogg.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Scanner;
 
-import no.hvl.dat100.jplab11.common.TODO;
+
 import no.hvl.dat100.jplab11.oppgave1.*;
 import no.hvl.dat100.jplab11.oppgave2.*;
 import no.hvl.dat100.jplab11.oppgave3.*;
@@ -21,8 +21,56 @@ public class LesBlogg {
 	private static String BILDE = "BILDE";
 
 	public static Blogg les(String mappe, String filnavn) {
+		int tabellLengde = 0;
+		File fil = new File(mappe + filnavn);
+		try {
+			
+			Scanner scanner = new Scanner(fil);
 
-		throw new UnsupportedOperationException(TODO.method());
+			if (scanner.hasNextLine()) {
+				tabellLengde = Integer.parseInt(scanner.nextLine());
+			}
 
+			Blogg blogg = new Blogg(tabellLengde);
+
+			while (scanner.hasNextLine()) {
+				String check = scanner.nextLine();
+				if (check.equals(TEKST)) {
+					int id = Integer.parseInt(scanner.nextLine());
+					String bruker = scanner.nextLine();
+					String dato = scanner.nextLine();
+					int likes = Integer.parseInt(scanner.nextLine());
+					String tekst = scanner.nextLine();
+
+					Tekst text = new Tekst(id, bruker, dato, likes, tekst);
+					blogg.leggTil(text);
+
+				}
+				if (check.equals(BILDE)) {
+					int id = Integer.parseInt(scanner.nextLine());
+					String bruker = scanner.nextLine();
+					String dato = scanner.nextLine();
+					int likes = Integer.parseInt(scanner.nextLine());
+					String tekst = scanner.nextLine();
+					String URL = scanner.nextLine();
+
+					Bilde bilde = new Bilde(id, bruker, dato, likes, tekst, URL);
+					blogg.leggTil(bilde);
+				}
+
+			}
+			scanner.close();
+			return blogg;
+
+		} catch (FileNotFoundException e) {
+			System.out.println("Kunne ikke finne angitt fil");
+			e.printStackTrace();
+			JOptionPane.showMessageDialog(null, "Kunne ikke finne angitt fil");
+		}catch (Exception e) {
+			System.out.println(e);
+			JOptionPane.showMessageDialog(null, "Det var et problem ved lesing av fil");
+		}
+
+		return null;
 	}
 }
